eceedd6101d379bc07ce21b9cf1177c114dc1b34
igoorodrigues <igoor.rodriguess@outlook.com>
2021-09-15 16:54:06 -0300

    Created the employee class

---
+++ employee_management/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=70)
+    department = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name, self.email, self.department
